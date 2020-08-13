40663441f9f6608658e3e3560cf873ffa68de29e
Lukyanenko Evgeny <juz@hive.ru>
2020-08-13 09:15:40 +0500

    WebRTC Соединение устанавливается, но звука нет. вызов MixedReality AddIceCandidate заработал только из main thread.

---
+++ ConsoleApp1/MyLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp1 {
+	public class MyLogger : ILogger {
+		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
+			var message = formatter(state, exception);
+
+			if (!string.IsNullOrEmpty(message) || exception != null) {
+				Console.WriteLine($"MyLogger: {message}");
+			}
+		}
+
+		public bool IsEnabled(LogLevel logLevel) {
+			return true;
+		}
+
+		public IDisposable BeginScope<TState>(TState state) {
+			return null;
+		}
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Kurento.NET;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.MixedReality.WebRTC;
 using IceCandidate = Microsoft.MixedReality.WebRTC.IceCandidate;
@@ -15,38 +15,38 @@ namespace ConsoleApp1 {
 		private static WebRtcEndpoint webRtcEndpoint;
 		private static PeerConnection peerConnection;
 		private static Transceiver audioTransceiver;
+		private static Queue<IceCandidate> kurentoIceCandidates = new Queue<IceCandidate>();
+		private static bool kurentoGathered;
+		private static bool kurentoReady;
+		private static bool clientReady;
 
 		public static async Task Main(string[] args) {
-			
-			
-			Console.WriteLine("Kurento side adjustment");
 			MyLogger logger = new MyLogger();
 			var kurentoClient = new KurentoClient("ws://hive.ru:8888/kurento");//, logger);
-			Console.WriteLine("Getting kurento server info");
-			var serverInfo = await kurentoClient.GetServerManager().GetInfoAsync();
-			Console.WriteLine($"Krento server version: {serverInfo.version}");
-
-			Console.WriteLine("Creating MediaPipeline");
+			Console.WriteLine("Kurento <- Getting server info");
+			var serverManager = kurentoClient.GetServerManager();
+			var serverInfo = await serverManager.GetInfoAsync();
+			Console.WriteLine($"Kurento -> server version: {serverInfo.version}");
+			
+			Console.WriteLine("Kurento <- Creating MediaPipeline...");
 			var pipeLine = await kurentoClient.CreateAsync(new MediaPipeline());
-			Console.WriteLine($"Pipeline created: {pipeLine.id}");
-			Console.WriteLine("Creating WebRTC Endpoint on Kurento");
+			Console.WriteLine($"Kurento -> Pipeline created: {pipeLine.id}");
+			Console.WriteLine("Kurento <- Creating WebRTC Endpoint...");
 			webRtcEndpoint = await kurentoClient.CreateAsync(new WebRtcEndpoint(pipeLine));
-			Console.WriteLine($"WebRTC Endpoint created: {webRtcEndpoint.id}");
-
+			Console.WriteLine($"Kurento -> WebRTC Endpoint created: {webRtcEndpoint.id}");
+			webRtcEndpoint.OnIceComponentStateChanged += WebRtcEndpointOnIceComponentStateChanged;
+			webRtcEndpoint.IceCandidateFound += WebRtcEndpointOnIceCandidateFound;
+			webRtcEndpoint.IceGatheringDone += WebRtcEndpointOnIceGatheringDone;
+			webRtcEndpoint.NewCandidatePairSelected += WebRtcEndpointOnNewCandidatePairSelected;
 			
 			// дока на метод Connect
 			// https://doc-kurento.readthedocs.io/en/stable/_static/client-javadoc/org/kurento/client/MediaElement.html#connect-org.kurento.client.MediaElement-
 			// Connects two elements, with the media flowing from left to right
-			Console.WriteLine("Connecting WebRTC Endpoint to itself");
+			Console.WriteLine("Kurento <- Connecting WebRTC Endpoint to itself");
 			await webRtcEndpoint.ConnectAsync(webRtcEndpoint, MediaType.AUDIO);
-			Console.WriteLine("WebRTC Endpoint connected to itself");
-			
+			Console.WriteLine("Kurento -> WebRTC Endpoint connected to itself");
 
-
-			Console.WriteLine("\n-------------------------------------------------------\n");
-
-			Console.WriteLine("Client side adjustment");
-			Console.WriteLine($"Creating PeerConnection");
+			Console.WriteLine($"Client: Creating PeerConnection...");
 
 			peerConnection = new PeerConnection();
 
@@ -54,6 +54,7 @@ namespace ConsoleApp1 {
 			peerConnection.LocalSdpReadytoSend += PeerConnectionOnLocalSdpReadytoSend;
 			peerConnection.IceStateChanged += PeerConnectionOnIceStateChanged;
 			peerConnection.AudioTrackAdded += PeerConnectionOnAudioTrackAdded;
+			peerConnection.IceGatheringStateChanged += PeerConnectionOnIceGatheringStateChanged;
 			peerConnection.IceCandidateReadytoSend += PeerConnectionOnIceCandidateReadytoSend;
 
 			var connectionConfig = new PeerConnectionConfiguration {
@@ -68,14 +69,14 @@ namespace ConsoleApp1 {
 
 			await peerConnection.InitializeAsync(connectionConfig);
 
-			Console.WriteLine($"peerConnection.Initialized = {peerConnection.Initialized}");
+			Console.WriteLine($"Client: peerConnection.Initialized = {peerConnection.Initialized}");
 
 			var microphoneConfig = new LocalAudioDeviceInitConfig {
 				AutoGainControl = true
 			};
 
 			var microphoneSource = await DeviceAudioTrackSource.CreateAsync(microphoneConfig);
-			Console.WriteLine("Microphone detected");
+			Console.WriteLine("Client: Microphone detected");
 
 			var localTrackConfig = new LocalAudioTrackInitConfig {
 				trackName = "microphone_track"
@@ -88,42 +89,121 @@ namespace ConsoleApp1 {
 			audioTransceiver.LocalAudioTrack = localAudioTrack;
 			audioTransceiver.DesiredDirection = Transceiver.Direction.SendReceive;
 
-			Console.WriteLine("AudioTransceiver configured with localAudioTrack");
+			Console.WriteLine("Client: AudioTransceiver configured with localAudioTrack");
 
 			var offerCreated = peerConnection.CreateOffer();
-			Console.WriteLine($"SDP offer created = {offerCreated}");
+			Console.WriteLine($"Client: SDP offer created = {offerCreated}");
+
+			while (!kurentoGathered) {
+				Thread.Sleep(100);
+			}
+			Console.WriteLine("Client: Dequeuing kurento ICE candidates...");
+
+			while (kurentoIceCandidates.Count > 0) {
+				var kurentoIceCandidate = kurentoIceCandidates.Dequeue();
+				Console.WriteLine($"Client: Adding Ice candidate: {kurentoIceCandidate.Content}");
+				peerConnection.AddIceCandidate(kurentoIceCandidate);
+				Console.WriteLine("Client: Ice candidate added");
+			}
+
+			while (!clientReady || !kurentoReady) {
+				Thread.Sleep(100);
+			}
+			
+			Console.WriteLine("Web RTC connection established!");
+			
+			
 
 			Console.ReadKey(true);
-			
+
 			localAudioTrack?.Dispose();
 			microphoneSource?.Dispose();
 			peerConnection.Dispose();
 		}
 
-		private static void PeerConnectionOnIceCandidateReadytoSend(IceCandidate candidate) {
-			Console.WriteLine("PeerConnectionOnIceCandidateReadytoSend:");
-			Console.WriteLine(candidate.Content);
-			
+		#region Kurento peer event handlers
+
+		private static void WebRtcEndpointOnIceComponentStateChanged(OnIceComponentStateChangedEventArgs args) {
+			Console.WriteLine($"Kurento -> IceComponentState changed to {args.state}. ({args.componentId}:{args.streamId})");
+			if (args.state == IceComponentState.READY) {
+				kurentoReady = true;
+			}
+
+		}
+
+		private static void WebRtcEndpointOnIceCandidateFound(IceCandidateFoundEventArgs args) {
+			Console.WriteLine("Kurento -> Ice candidate found:");
+			var kurentoFormattedCandidate = args.candidate;
+			Console.WriteLine(kurentoFormattedCandidate.candidate);
+
+			var iceCandidate = new IceCandidate {
+				Content = kurentoFormattedCandidate.candidate,
+				SdpMid = kurentoFormattedCandidate.sdpMid,
+				SdpMlineIndex = kurentoFormattedCandidate.sdpMLineIndex
+			};
+
+			kurentoIceCandidates.Enqueue(iceCandidate);
+			Console.WriteLine("Client: Kurento Ice candidate enqueued.");
+			// Thread.Sleep(5000);
+			// Console.WriteLine("Client: Kurento Ice candidate adding...");
+			// peerConnection.AddIceCandidate(iceCandidate);
+			// Console.WriteLine("Client: Kurento Ice candidate added.");
+		}
+
+		private static void WebRtcEndpointOnIceGatheringDone(IceGatheringDoneEventArgs args) {
+			Console.WriteLine("Kurento -> Ice gathering done.");
+			kurentoGathered = true;
+		}
+
+		private static void WebRtcEndpointOnNewCandidatePairSelected(NewCandidatePairSelectedEventArgs args) {
+			Console.WriteLine($"Kurento -> New ICE candidate pair selected: {args.candidatePair.localCandidate} <-> {args.candidatePair.localCandidate})");
+		}
+
+		#endregion
+
+		#region Client peer event handlers
+
+		private static void PeerConnectionOnIceGatheringStateChanged(IceGatheringState newState) {
+			Console.WriteLine(value: $"Client: ICE candidates gathering state: {newState}");
+		}
+
+		private static void PeerConnectionOnIceCandidateReadytoSend(IceCandidate clientIceCandidate) {
+			Console.WriteLine("Client: client ICE candidate found:");
+			Console.WriteLine(clientIceCandidate.Content);
+			var kurentoFormattedIceCandidate = new Kurento.NET.IceCandidate {
+				candidate = clientIceCandidate.Content,
+				sdpMid = clientIceCandidate.SdpMid,
+				sdpMLineIndex = clientIceCandidate.SdpMlineIndex
+			};
+			Console.WriteLine("Kurento <- Adding client ICE candidate...");
+			webRtcEndpoint.AddIceCandidateAsync(kurentoFormattedIceCandidate).Wait();
+			Console.WriteLine("Kurento -> client ICE candidate added");
 		}
 
 		private static void PeerConnectionOnLocalSdpReadytoSend(SdpMessage message) {
-			Console.WriteLine("Local SDP offer:");
+			Console.WriteLine("Client: Local SDP offer:");
 			Console.WriteLine(message.Content);
-			Console.WriteLine("Sending offer to Kurento...");
+			Console.WriteLine("Kurento <- Sending client SDP offer...");
 			var sdpAnswerAsync = webRtcEndpoint.ProcessOfferAsync(message.Content);
 			sdpAnswerAsync.Wait();
-			Console.WriteLine("SDP Answer received:");
+			Console.WriteLine("Client: SDP answer received:");
 			var sdpAnswer = sdpAnswerAsync.Result;
 			Console.WriteLine(sdpAnswer);
-			Console.WriteLine("Applying answer to local peerConnection");
+			Console.WriteLine("Client: Applying answer to local peerConnection...");
 			var sdpAnswerMessage = new SdpMessage() {
 				Content = sdpAnswer,
 				Type = SdpMessageType.Answer
 			};
 			var remoteDescriptionAsync = peerConnection.SetRemoteDescriptionAsync(sdpAnswerMessage);
 			remoteDescriptionAsync.Wait();
-			Console.WriteLine("SDP Answer accepted");
-			Console.WriteLine($"Remote track detected: {audioTransceiver.RemoteTrack.Name}");
+			Console.WriteLine("Client: SDP answer accepted");
+			Console.WriteLine($"Client: Remote track detected: {audioTransceiver.RemoteTrack.Name}");
+
+			
+			Console.WriteLine("Kurento <- Starting ICE candidates gathering...");
+			webRtcEndpoint.GatherCandidatesAsync().Wait();
+			Console.WriteLine("Kurento -> Started ICE candidates gathering");
+			
 		}
 
 		private static void PeerConnectionOnAudioTrackAdded(RemoteAudioTrack remoteTrack) {
@@ -131,6 +211,7 @@ namespace ConsoleApp1 {
 		}
 
 		private static void RemoteTrackOnAudioFrameReady(AudioFrame frame) {
+			if (!clientReady || !kurentoReady) return;
 			++numFrames;
 			// var bytes = new byte[frame.sampleCount*2];
 			// Marshal.Copy(frame.audioData, bytes, 0, bytes.Length);
@@ -140,35 +221,23 @@ namespace ConsoleApp1 {
 			// }
 			// Console.WriteLine(maxByte);
 			if (numFrames % 100 == 0) {
-				Console.WriteLine($"Received audio frames: {numFrames}");
+				Console.WriteLine($"Client: Received audio frames: {numFrames}");
 			}
 		}
 
 		private static void PeerConnectionOnConnected() {
-			Console.WriteLine("PeerConnection: connected.");
+			Console.WriteLine("Client: peerConnection connected.");
 		}
 
 		private static void PeerConnectionOnIceStateChanged(IceConnectionState newState) {
-			Console.WriteLine($"ICE state: {newState}");
-		}
-	}
-
-	public class MyLogger : ILogger {
-		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) {
-			var message = formatter(state, exception);
-
-			if (!string.IsNullOrEmpty(message) || exception != null) {
-				Console.WriteLine($"MyLogger: {message}");
+			Console.WriteLine($"Client: ICE state: {newState}");
+			if (newState == IceConnectionState.Completed) {
+				clientReady = true;
 			}
 		}
 
-		public bool IsEnabled(LogLevel logLevel) {
-			return true;
-		}
+		#endregion
 
-		public IDisposable BeginScope<TState>(TState state) {
-			return null;
-		}
 	}
 
 }
